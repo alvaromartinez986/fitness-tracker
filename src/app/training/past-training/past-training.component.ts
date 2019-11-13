9bef65db06b791e5119c7061782659c9e5fa10ac
Alvaro Martinez <amartine@pros.com>
2019-11-13 08:37:34 -0500

    Connect store with the component for past trainings

--- src/app/training/past-training/past-training.component.ts
+++ src/app/training/past-training/past-training.component.ts
@@ -1,36 +1,33 @@
-import {
-  Component,
-  OnInit,
-  ViewChild,
-  AfterViewInit,
-  OnDestroy
-} from "@angular/core";
+import { Component, OnInit, ViewChild, AfterViewInit } from "@angular/core";
 import { MatTableDataSource, MatSort, MatPaginator } from "@angular/material";
 import { Exercise } from "../exercise.model";
 import { TrainingService } from "../training.service";
-import { Subscription } from "rxjs";
+import { Store } from "@ngrx/store";
+import * as fromTraining from "../training.reducer";
 
 @Component({
   selector: "app-past-training",
   templateUrl: "./past-training.component.html",
   styleUrls: ["./past-training.component.css"]
 })
-export class PastTrainingComponent implements OnInit, AfterViewInit, OnDestroy {
+export class PastTrainingComponent implements OnInit, AfterViewInit {
   displayedColumns = ["date", "name", "duration", "calories", "state"];
   dataSource = new MatTableDataSource<Exercise>();
-  private exChangedSubscription: Subscription;
 
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  constructor(private trainningService: TrainingService) {}
+  constructor(
+    private trainningService: TrainingService,
+    private store: Store<fromTraining.State>
+  ) {}
 
   ngOnInit() {
-    this.exChangedSubscription = this.trainningService.finishedExercisesChanged.subscribe(
-      (exercises: Exercise[]) => {
+    this.store
+      .select(fromTraining.getFinishedExercises)
+      .subscribe((exercises: Exercise[]) => {
         this.dataSource.data = exercises;
-      }
-    );
+      });
     this.trainningService.fetchCompletedOrCancelledExercises();
   }
 
@@ -41,11 +38,5 @@ export class PastTrainingComponent implements OnInit, AfterViewInit, OnDestroy {
 
   doFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
-  }
-
-  ngOnDestroy() {
-    if (this.exChangedSubscription) {
-      this.exChangedSubscription.unsubscribe();
-    }
   }
 }
